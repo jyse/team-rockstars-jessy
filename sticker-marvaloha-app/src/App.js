feecc544c31ae92a9d43a8d6229377b1d09ed131
Jessy The <jessythe@Jessys-MacBook-Pro.local>
2021-10-06 19:28:51 +0200

    first commit

---
+++ sticker-marvaloha-app/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./App.css";
+import { IceCream, SpeechBubble, Chocolate } from "react-kawaii";
+import stickersData from "./stickersData";
+import { Rnd } from "react-rnd";
+
+export default function App() {
+  // const [images, setImages] = useState([]);
+
+  return (
+    <div className="app">
+      <img src="https://images.unsplash.com/photo-1609258612794-3502fb4306a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" />
+      <div className="stickers">
+        <h4 className="heading"> Click/Tap to add sticker to photo!</h4>
+        {stickersData.map((sticker) => {
+          return (
+            <div className="sticker">
+              <Rnd>
+                <sticker.name
+                  size={150}
+                  mood={sticker.mood}
+                  color={sticker.color}
+                />
+              </Rnd>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ sticker-marvaloha-app/src/stickersData.js
@@ -0,0 +1,33 @@
+import { Mug, IceCream, SpeechBubble, Chocolate } from "react-kawaii";
+
+const stickersData = [
+  {
+    name: IceCream,
+    color: "#FDA7DC",
+    mood: "lovestruck",
+    width: 100,
+    alt: "IceCream",
+  },
+  {
+    name: Chocolate,
+    color: "#EF5B5B",
+    mood: "blissful",
+    width: 150,
+    alt: Chocolate,
+  },
+  {
+    name: SpeechBubble,
+    color: "#ADD9F4",
+    mood: "sad",
+    width: 60,
+    alt: "Speech Bubble",
+  },
+  {
+    name: Mug,
+    color: "#A6E191",
+    mood: "shocked",
+    width: 60,
+    alt: "Mug",
+  },
+];
+export default stickersData;
