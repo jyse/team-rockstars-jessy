d3b278b1132c833de11bdc7f0e64784e1c988701
Jessy The <jessythe@Jessys-MacBook-Pro.local>
2021-10-06 22:37:00 +0200

    first real commit

--- sticker-marvaloha-app/src/App.js
+++ sticker-marvaloha-app/src/App.js
@@ -11,6 +11,7 @@ export default function App() {
     <div className="app">
       <img src="https://images.unsplash.com/photo-1609258612794-3502fb4306a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" />
       <div className="stickers">
+        <h1>HELLO STICKER </h1>
         <h4 className="heading"> Click/Tap to add sticker to photo!</h4>
         {stickersData.map((sticker) => {
           return (
